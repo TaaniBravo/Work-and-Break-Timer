149b98ba8057a9cf39a4aef0388ea8eba45506c8
Taani Maama <taani1maama@live.com>
2021-05-16 20:17:02 -0700

    Fixed the issue where the clock wouldn't show the break timer at starting time.

--- src/components/Session.js
+++ src/components/Session.js
@@ -3,52 +3,58 @@ import { useCountsContext } from "../hooks/useCounts";
 
 const Session = () => {
   const [counts, setCounts] = useCountsContext();
+  const { workCount, breakCount, onBreak, displayTime } = counts;
 
   let countdown = 0;
   let interval;
 
-  console.log(interval);
-
   const sessionTimer = () => {
-    if (counts.workCount === countdown) {
+    if (workCount === countdown) {
       clearInterval(interval);
+      interval = null;
+
+      setCounts({ ...counts, onBreak: true });
+
       countdown = 0;
-      interval = setInterval(breakTimer, 1000);
+
+      document.getElementById("timer-label").innerHTML = "Break Time!";
+      document.getElementById("time-left").innerHTML = formatTimer(breakCount);
+      setTimeout(() => {
+        interval = setInterval(breakTimer, 1000);
+      }, 1000);
+    } else {
+      countdown++;
+      document.getElementById("time-left").innerHTML = formatTimer(
+        workCount - countdown
+      );
     }
-
-    console.log();
-
-    countdown++;
-    document.getElementById("time-left").innerHTML = formatTimer(
-      counts.workCount - countdown
-    );
   };
 
   const breakTimer = () => {
-    if (counts.breakCount === countdown) {
+    if (breakCount === countdown) {
       clearInterval(interval);
       interval = null;
       resetTimer();
+    } else {
+      countdown++;
+      document.getElementById("time-left").innerHTML = formatTimer(
+        breakCount - countdown
+      );
     }
-
-    countdown++;
-    document.getElementById("time-left").innerHTML = formatTimer(
-      counts.breakCount - countdown
-    );
   };
 
   const start = () => {
-    interval = setInterval(
-      counts.workCount !== 0 ? sessionTimer : breakTimer,
-      1000
-    );
-    console.log(interval);
+    if (!interval) {
+      !onBreak
+        ? (interval = setInterval(sessionTimer, 1000))
+        : (interval = setInterval(breakTimer, 1000));
+    }
   };
 
   const pause = () => {
     clearInterval(interval);
     interval = null;
-    countdown -= 3;
+    countdown -= 4;
   };
 
   const resetTimer = () => {
@@ -59,32 +65,39 @@ const Session = () => {
 
     setCounts({
       workCount: 1500,
-      breakCount: 300
+      breakCount: 300,
+      onBreak: false
     });
 
+    document.getElementById("timer-label").innerHTML = "Session";
+
     document.getElementById("time-left").innerHTML = formatTimer(
-      counts.workCount - countdown
+      workCount - countdown
     );
   };
 
   const formatTimer = seconds => {
-    let sec = seconds % 60;
     let min = parseInt(seconds / 60);
+    let sec = seconds % 60;
 
     if (min.toString().length === 1) min = "0" + min;
     if (sec.toString().length === 1) sec = "0" + sec;
+
+    console.log(min + ":" + sec);
 
     return min + ":" + sec;
   };
 
   const handleStartStop = () => {
+    console.log(interval);
     !interval ? start() : pause();
+    console.log(interval);
   };
 
   return (
     <div>
       <h4 id="timer-label">Session</h4>
-      <h2 id="time-left">{formatTimer(counts.workCount - countdown)}</h2>
+      <h2 id="time-left">{formatTimer(workCount - countdown)}</h2>
       <div className="button-container">
         <button onClick={handleStartStop} id="start_stop">
           <i class="fas fa-play"></i>
--- src/hooks/useCounts.js
+++ src/hooks/useCounts.js
@@ -5,8 +5,10 @@ const { Provider } = CountContext;
 
 const CountsProvider = ({ value = [], ...props }) => {
   const [counts, setCounts] = useState({
-    workCount: 1500,
-    breakCount: 300
+    workCount: 1,
+    breakCount: 300,
+    onBreak: false,
+    displayTime: 1
   });
 
   return <Provider value={[counts, setCounts]} {...props} />;
